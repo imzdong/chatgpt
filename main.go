223090a84ace257be082a8bc86d98976850c4723
Winter <zhoudong_dream@163.com>
2023-04-07 22:41:40 +0800

    init go web

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, nil)
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fmt.Println("Listening on :8080...")
+	http.ListenAndServe(":8080", nil)
+}
