878de549f02107cd61842c38e1f6a36880e3193b
Winter <zhoudong_dream@163.com>
2023-04-08 11:53:25 +0800

    vue request go success

--- main.go
+++ main.go
@@ -19,6 +19,17 @@ func main() {
 	http.HandleFunc("/", indexHandler)
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	// 添加路由信息
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		// 设置跨域访问权限
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// 处理 API 请求
+		fmt.Fprint(w, "Hello, API!")
+	})
+
 	fmt.Println("Listening on :8086...")
 	http.ListenAndServe(":8086", nil)
 }
